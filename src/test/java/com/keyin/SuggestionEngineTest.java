9084e4691621ff68e8b46dc5cd4301a4bc39830a
Matt Young <127060152+myoung24@users.noreply.github.com>
2024-01-30 15:20:00 -0330

    added sample test failed

---
+++ src/test/java/com/keyin/SuggestionEngineTest.java
@@ -0,0 +1,67 @@
+package com.keyin;
+
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.Map;
+
+@ExtendWith(MockitoExtension.class)
+public class SuggestionEngineTest {
+    private SuggestionEngine suggestionEngine = new SuggestionEngine();
+
+    @Mock
+    private SuggestionsDatabase mockSuggestionDB;
+    private boolean testInstanceSame = false;
+
+    @Test
+    public void testGenerateSuggestions() throws Exception {
+        suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
+
+//        Assertions.assertTrue(testInstanceSame);
+        Assertions.assertTrue(suggestionEngine.generateSuggestions("hellw").contains("hello"));
+    }
+
+    @Test
+    public void testGenerateSuggestionsFail() throws Exception {
+        suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
+
+        testInstanceSame = true;
+        Assertions.assertTrue(testInstanceSame);
+        Assertions.assertFalse(suggestionEngine.generateSuggestions("hello").contains("hello"));
+    }
+
+    @Test
+    public void testSuggestionsAsMock() {
+        Map<String,Integer> wordMapForTest = new HashMap<>();
+
+        wordMapForTest.put("test", 1);
+
+        Mockito.when(mockSuggestionDB.getWordMap()).thenReturn(wordMapForTest);
+
+        suggestionEngine.setWordSuggestionDB(mockSuggestionDB);
+
+        Assertions.assertFalse(suggestionEngine.generateSuggestions("test").contains("test"));
+
+        Assertions.assertTrue(suggestionEngine.generateSuggestions("tes").contains("test"));
+    }
+
+    @Test
+    public void notReplaceWord() throws Exception {
+        suggestionEngine.loadDictionaryData( Paths.get( ClassLoader.getSystemResource("words.txt").getPath()));
+
+        // if word exists, similar words should not be suggested
+        Assertions.assertFalse(suggestionEngine.generateSuggestions("form").contains("fort"));
+    }
+
+    @Test
+    public void sampleTest () {
+        Assertions.assertTrue(testInstanceSame);
+    }
+}
